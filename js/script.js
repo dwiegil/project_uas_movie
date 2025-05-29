55a2817b4743546c1d3feef1dcff524af13fa4ad
dwiegil <dwiegilradhiatur@gmail.com>
2025-05-29 11:34:22 +0700

    fifth commit

--- js/script.js
+++ js/script.js
@@ -67,16 +67,20 @@ $('#movie-list').on('click', '.see-detail', function () {
             if( movie.Response === "True") {
 
 
-                $('#modal-body').html(`
+                $('.modal-body').html(`
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-md-4">
-                                <img src="${data.Poster}" class="img-fluid">
+                                <img src="${movie.Poster}" class="img-fluid">
                             </div>
 
                             <div class="col-md-8">
                                 <ul class="list-group">
-                                <li class="list-group-item"><h3>${data.Title}</h3></li>
+                                    <li class="list-group-item"><h3>${movie.Title}</h3></li>
+                                    <li class="list-group-item">Released : ${movie.Released}</li>
+                                    <li class="list-group-item">Genre : ${movie.Genre}</li>
+                                    <li class="list-group-item">Director : ${movie.Director}</li>
+                                    <li class="list-group-item">Actors : ${movie.Actors}</li>
                                 </ul>
                             </div>
                     </div>
